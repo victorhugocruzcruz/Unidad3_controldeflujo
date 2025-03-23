471d6ff988b80fb5904d66b39c0d4970cd45c841
victorhugocruzcruz <cruzcruzvictorhugo724@gmail.com>
2025-03-23 13:33:12 -0500

    Add files via upload

---
+++ Practica8Fact/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Practica8Fact
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            button1 = new Button();
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Calibri", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(277, 33);
+            label1.TabIndex = 0;
+            label1.Text = "Factorial de un Número";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(192, 255, 192);
+            button1.Cursor = Cursors.Hand;
+            button1.Font = new Font("Calibri", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(45, 147);
+            button1.Name = "button1";
+            button1.Size = new Size(195, 51);
+            button1.TabIndex = 1;
+            button1.Text = "CALCULAR";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.Cursor = Cursors.No;
+            listBox1.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 19;
+            listBox1.Location = new Point(311, 36);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(120, 308);
+            listBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.Cursor = Cursors.IBeam;
+            textBox1.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(45, 88);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(194, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Calibri", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.Location = new Point(45, 365);
+            label2.Name = "label2";
+            label2.Size = new Size(194, 23);
+            label2.TabIndex = 4;
+            label2.Text = "El Factorial del Número";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Calibri", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label3.Location = new Point(355, 365);
+            label3.Name = "label3";
+            label3.Size = new Size(26, 23);
+            label3.TabIndex = 5;
+            label3.Text = "es";
+            // 
+            // textBox2
+            // 
+            textBox2.Cursor = Cursors.No;
+            textBox2.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(257, 361);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(83, 27);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Cursor = Cursors.No;
+            textBox3.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(396, 361);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(112, 27);
+            textBox3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Teal;
+            ClientSize = new Size(530, 422);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Factorial de un numero";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox2;
+        private TextBox textBox3;
+    }
+}
---
+++ Practica8Fact/Form1.cs
@@ -0,0 +1,23 @@
+namespace Practica8Fact
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numero = int.Parse(textBox1.Text);
+            int factorial = 1;
+            for ( int i = 1; i<= numero; i++)
+            {
+                listBox1.Items.Add(i);
+                factorial *= i;
+            }
+            textBox2.Text = numero.ToString();
+            textBox3.Text = factorial.ToString();
+        }
+    }
+}
